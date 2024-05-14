04bacb63d6bc29b4a18f95f94471f8c974fcfbb1
Zaid Hilman <mannmarpaung@gmail.com>
2024-05-14 13:36:11 +0700

    3

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -111,7 +111,7 @@ class BookController extends Controller
             
             $book->update($data);
 
-            // dd($book);2
+            // dd($book);3
 
             return redirect()->route('book.index');
             
